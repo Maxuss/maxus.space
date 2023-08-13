9bdce76c9bf70766a98dbdc1a82906a1a4a30e39
Maxuss <maxus.info.mail@gmail.com>
2023-08-13 14:11:19 +0300

    Fixed double rendering of components

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {GlobalStyle, ThemeProvider} from "@react95/core";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
       <ThemeProvider theme={"storm"}>
           <GlobalStyle />
           <App />
       </ThemeProvider>
-  </React.StrictMode>,
+  </>,
 )
