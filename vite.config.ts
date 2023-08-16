52eca9916bf031f95ec2d97a52adc212bdc89ed8
Maxuss <maxus.info.mail@gmail.com>
2023-08-16 12:00:14 +0300

    improved build chunking

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (id.includes("@react95")) {
+              return "vendor_react95";
+            }
+
+            return "vendor"; // all other package goes here
+          }
+        },
+      }
+    }
+  }
 })
